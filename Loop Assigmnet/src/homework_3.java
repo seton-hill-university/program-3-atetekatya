1fb9c7c4c3e60336c1b838f505eb656a49dc2754
atetekatya <112852372+atetekatya@users.noreply.github.com>
2022-09-28 00:45:29 -0400

    Done.

--- Loop Assigmnet/src/homework_3.java
+++ Loop Assigmnet/src/homework_3.java
@@ -1,5 +1,5 @@
 
-// Application: Homework 
+// Application: Homework3
 // Name: Katya Atete
 // GitHub User: atetekatya
 // Date: Sep 22, 2022
@@ -8,35 +8,54 @@
 
 
 import java.util.*;
+
 public class homework_3 {
 
 	public static void main(String[] args) {
 		
+		
 		//Initialize Scanner and variables
 		
-		Scanner user = new Scanner(System.in);
+				Scanner user = new Scanner(System.in);
+				
+				String name;
+				int num;
+				
+				// Ask the user to enter their name and numbers between 0 and 100
+				
+				System.out.println("Enter User Names");
+				name = user.next();
+				
+				System.out.println("Enter a number");
+				num = user.nextInt();
+				
+				//Check if the number is between 0 and 100
+				
+				while (!(num>0 && num<100))
+				{
+					System.out.println("Please re-enter the number " +name);
+					num= user.nextInt();
 		
-		String name;
-		int num;
-		
-		// Ask the user to enter their name and numbers between 0 and 100
-		
-		System.out.println("Enter User Names");
-		name = user.next();
-		
-		System.out.println("Enter a number");
-		num = user.nextInt();
-		
-		do
-			System.out.println("Enter another number");
-			
-		
-		while (num<100 && num>0);
-		
-			System.out.println("Thank you for your input " +name);
-		
-		
-		
-	}
+				
+					if (num>0 && num<100)
+						System.out.println("Thank you for your input " +name);
 
-}
+					
+					 
+				}
+				
+				System.out.println("Thank you for your input " +name);
+				
+				// Count down the numbers from user input
+
+				
+				for (int i=num;i>=0;i--)
+				{
+					System.out.println(+i);
+					System.out.println("Your doing it " +name);
+				}
+				
+				
+			}
+		
+		}
